142b260ade2a21b95221f97b90bf7e4e4e48ddbf
piotr-jablonski <RENT@192.168.1.18>
2017-04-04 18:29:16 +0200

    First commit

---
+++ src/main/java/pl/sda/course/TaxCalculator/App.java
@@ -0,0 +1,13 @@
+package pl.sda.course.TaxCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
